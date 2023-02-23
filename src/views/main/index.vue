dcaed87a0430a7412a683284b5e43f1512ce01f8
yu <yuu9916@gmail.com>
2023-02-23 12:09:28 +0800

    refactor : Split completed components into files

---
+++ src/views/main/index.vue
@@ -0,0 +1,8 @@
+<script>
+export default {
+  name: 'app',
+};
+</script>
+<!-- 引入template.html,style.scss  -->
+<template src="./template.html"></template>
+<style lang="scss" src="./style.scss" scoped></style>
