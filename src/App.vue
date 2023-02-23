2f942732ce5d026853e38c880d98949991a6aeef
yu <yuu9916@gmail.com>
2023-02-23 09:36:20 +0800

    add : basic layout

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,30 @@
 <template>
   <div id="app">
-    <nav>
+    <!-- <nav>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </nav>
-    <router-view/>
+    <router-view /> -->
+    <el-container>
+      <el-header class="el-header">header</el-header>
+      <el-container class="container">
+        <el-aside class="el-aside">a-side</el-aside>
+        <el-main class="el-main">dlkfld</el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+
+.el-header {
+  background-color: rgb(29, 54, 87);
+}
+.el-aside {
+  background-color: rgb(95, 124, 124);
+}
+.container {
+  height: calc(100vh - 60px);
 }
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js Appsdd" />
   </div>
 </template>
 
+<style scoped>
+
+</style>
+
 <script>
-// @ is an alias to /src
+
 import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
